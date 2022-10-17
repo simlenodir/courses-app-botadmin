8dbbf98980b3bcbd255f21d0ff21c7b1779ef1a8
simlenodir <nodirsmailov0@gmail.com>
2022-10-17 16:36:22 +0300

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://course-bot-again.herokuapp.com/courseRequest")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((err) => console.log(err));
+    }, [data]);
+  return (
+    <div className="App container">
+      <h3 className="text-center">Admin page of Courses </h3>
+      <ul>
+        {data.length &&
+          data.map((e, i) => (
+            <li key={i}>
+              <h4>{e.name}</h4>
+              <p>{e.location}</p>
+              <span>{e.phone}</span>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
