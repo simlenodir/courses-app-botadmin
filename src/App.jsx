ec0ada5c99099116a4359cce9383bbde8409d4dd
simlenodir <nodirsmailov0@gmail.com>
2022-10-17 17:20:55 +0300

    not changed

--- src/App.jsx
+++ src/App.jsx
@@ -15,22 +15,10 @@ function App() {
         {data.length &&
           data.map((e, i) => (
             <li key={i}>
-              <div className="card" style="width:400px">
-                <div className="card-body">
-                  <h4 className="card-title">{e.name}</h4>
-                  <p className="card-text">
-                    {e.location}
-                  </p>
-                  <span>{e.phone}</span>
-                  <a href="#" className="btn btn-primary">
-                    See Profile
-                  </a>
-                </div>
-              </div>
-              {/* <h4>{e.name}</h4>
+              <h4>{e.name}</h4>
               <p>{e.location}</p>
               <span>{e.phone}</span>
-              <button className="btn btn-success-primary"></button> */}
+              <button className="btn btn-success-primary"></button>
             </li>
           ))}
       </ul>
