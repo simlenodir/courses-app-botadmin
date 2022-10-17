fb9f7e2589e25511ebe1a08d9bd2379c2d750736
simlenodir <nodirsmailov0@gmail.com>
2022-10-17 18:25:29 +0300

    fixed reacstrap cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import { useEffect, useState } from "react";
-import 'bootstrap/dist/css/bootstrap.css';
-import { Button } from 'reactstrap';
+import "bootstrap/dist/css/bootstrap.css";
+import {
+  CardLink,
+  ListGroup,
+  ListGroupItem,
+  CardTitle,
+  CardBody,
+  Card,
+} from "reactstrap";
 
 function App() {
   const [data, setData] = useState([]);
@@ -14,14 +21,29 @@ function App() {
   return (
     <div className="App container">
       <h3 className="text-center">Admin page of Courses </h3>
-      <ul>
+      <ul className="d-flex flex-wrap justify-content-around my-3">
         {data.length &&
           data.map((e, i) => (
-            <li key={i}>
-              <h4>{e.name}</h4>
-              <p>{e.location}</p>
-              <span>{e.phone}</span><br />
-              <Button> button</Button>
+            <li className="list-unstyled mx-1 my-3" key={i}>
+              <Card
+                style={{
+                  width: "16rem",
+                }}
+              >
+                <CardBody>
+                  <CardTitle tag="h5">{e.name}</CardTitle>
+                </CardBody>
+                <ListGroup flush style={{
+                  height: "10rem",
+                }}>
+                  <ListGroupItem>phone: {e.phone}</ListGroupItem>
+                  <ListGroupItem>location: {e.location}</ListGroupItem>
+                </ListGroup>
+                <CardBody>
+                  <CardLink href="#">Card Link</CardLink>
+                  <CardLink href="#">Another Card Link</CardLink>
+                </CardBody>
+              </Card>
             </li>
           ))}
       </ul>
